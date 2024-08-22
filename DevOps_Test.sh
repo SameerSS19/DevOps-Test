d60f96c2b6e560ced2cdeb4045f852f693521174
Shakiv Ali <56513506+shak160@users.noreply.github.com>
2024-08-22 14:57:51 +0530

    Network Monitoring

--- DevOps_Test.sh
+++ DevOps_Test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Display top 10 most used applications
-display_top_apps() {
-  echo -e "Most Used 10 Applications(CPU):"
-  ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 11
-  echo "Most used 10 Applications(Memory)"
-  ps -eo pid,comm,%cpu,%mem --sort=-%mem | head -n 11
+# Display network monitoring
+display_network() {
+  echo -e "Network Monitoring:"
+# Concurrent Connections
+  echo -e "Number of Concurrent Connections:"
+# Replace '80' with the port number you want to monitor
+  ss -s | grep 'ESTAB' | awk '{print $2 " ESTABLISHED connections"}'
+# Packet Drops
+  echo -e "Packet Drops:"
+# Replace 'eth0' with your actual network interface if different
+  netstat -i | awk '/^eth0/ {print "Received: " $3 " Transmitted: " $7 " (Packets)"}'
+  echo -e "\nNumber of MB In and Out:"
+  vnstat --oneline | awk -F';' '{print "In: " $10 " Out: " $12}'
 }
+
