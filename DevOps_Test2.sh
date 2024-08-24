4147011e4c48551f5febc40ffc436bc1517cc3da
Shakiv Ali <56513506+shak160@users.noreply.github.com>
2024-08-24 11:49:24 +0530

    File and Directory Permissions

--- DevOps_Test2.sh
+++ DevOps_Test2.sh
@@ -13,3 +13,18 @@ run_and_log "awk -F: '$3 == 0 {print \$1}' /etc/passwd" "Users with UID 0 (root 
 run_and_log "grep -E '^[^:]+:[^:]*$' /etc/shadow" "Users without passwords"
 run_and_log "find / -perm -04000 -type f" "Files with SUID bits set"
 run_and_log "find / -perm -02000 -type f" "Files with SGID bits set"
+
+
+# File and Directory Permissions
+{
+    echo "File and Directory Permissions"
+    echo "World-Writable Files and Directories"
+    find / -perm -0002 -type f -o -type d 2>/dev/null
+    echo
+    echo ".ssh Directory Permissions"
+    find /home -name ".ssh" -type d -exec ls -ld {} \; 2>/dev/null
+    echo
+    echo "Files with SUID or SGID Bits Set"
+    find / -perm -4000 -o -perm -2000 -type f 2>/dev/null
+    echo
+} >> "$REPORT_FILE"
